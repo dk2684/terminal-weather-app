3e5b3f70246b4d798dcb4536de861c9e8d931a5c
dk2684 <83376393+dk2684@users.noreply.github.com>
2022-08-04 23:54:08 -0700

    formatting update

--- api_key.py
+++ api_key.py
@@ -2,7 +2,6 @@
 # Separate Private Key from Public Files to Prevent Abuse (API Calls can be expensive)
 # or Encrypt file with execute-only permissions
 
-
 def return_key():
     private = "0ebd041b7d229c67d35cc631953db99a"
     return private
